bacc716504199b2d02a38393e4045013f59089c8
Jhonnathan0314 <jffc314@gmail.com>
2024-10-16 21:48:39 -0500

    Empty project

---
+++ src/test/java/com/easy/finance/EasyFinanceApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFinanceApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
